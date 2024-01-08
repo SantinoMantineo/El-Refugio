f007e7d386f71be16983587d681cc6e6f320187d
Santino Mantineo <santinomantineo@gmail.com>
2024-01-08 02:07:37 -0300

    Landing

--- Client/src/components/Landing/Landing.jsx
+++ Client/src/components/Landing/Landing.jsx
@@ -1,12 +1,18 @@
 // Landing.js
 import React from "react";
 import style from "./Landing.module.css";
-import Camping from "../../assets/camping.jpg";
 
 const Landing = () => {
   return (
     <div className={style.container} id="landing-section">
-      
+      <div className={style.text}>
+        <h1><span>El Refugio</span>, tu destino perfecto para desconectar de la rutina y conectar con la naturaleza.</h1>
+      </div>
+      <div className={style.reservar}>
+        <button>
+          Reservar
+        </button>
+      </div>
     </div>
   );
 };
